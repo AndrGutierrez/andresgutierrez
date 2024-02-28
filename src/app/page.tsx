fa092c9dc37786fc384ba353efc6536993b1cada
Andres Gutierrez <andresgutierrezdev@gmail.com>
2024-02-28 14:42:56 -0400

    adding "my clients section"

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,10 +3,40 @@ import { useEffect, useRef, useState, lazy, Suspense } from "react"
 import { motion, useAnimation, useInView } from "framer-motion";
 import { useGLTF, useAnimations } from "@react-three/drei";
 import { Canvas } from "@react-three/fiber";
-
+import Image from "next/image";
 const Atom = lazy(()=> import("@/components/Atom"));
 
 
+function Clients(){
+  const ref=useRef(null)
+  const mainControls= useAnimation();
+  const isInView = useInView(ref);
+  useEffect(()=>{
+    if (isInView) mainControls.start("visible")
+  }, [isInView])
+  return (
+    <section ref={ref} className="px-10 lg:px-20">
+        <motion.div className="w-100 " variants={{
+              hidden: {
+                opacity: 0, y: 75
+              },
+              visible: {
+                opacity: 1, y: 0
+              }
+            }} 
+            transition={{ duration: .7, delay: 0.3 }}
+            initial= "hidden"
+            animate={mainControls}
+            >
+
+          <h2 className="text-4xl mb-8">My Clients</h2>
+          <div className="flex justify-evenly align-center w-100">
+            <img src="images/clients/gobuyside.png" alt="goBuySide" />
+            <img src="images/clients/zeitfur.svg" alt="Zeit Fui Die Schule" className="invert" />
+          </div>
+        </motion.div>
+    </section>)
+}
 
 export default function Home() {
   const [title, setTitle] = useState(<>Hi, I&apos;m Andres</>);  
@@ -38,7 +68,7 @@ export default function Home() {
             }} 
             transition={{ duration: 1, delay: 0.3 }}
             initial= "hidden"
-            animate="visible"
+            animate={mainControls}
             onAnimationComplete={()=>{setDisplay(true)}}
             >
             <div className="mb-24 flex flex-col items-center lg:items-start">
@@ -63,6 +93,7 @@ export default function Home() {
             </div>
         </div>
       </div>
+      <Clients></Clients>
     </main>
   );
 }
