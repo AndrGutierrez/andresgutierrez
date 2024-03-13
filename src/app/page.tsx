07015d01aa2c0be0c9c287bb98ca1a1d0bcc1b14
Andres Gutierrrez <andresgutierrezdev@gmail.com>
2024-03-13 14:51:19 -0400

    adding animation end validation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,22 @@
 "use client"
-import { useEffect, useRef, useState, lazy, Suspense } from "react"
+import { useEffect, useRef, useState, lazy, Suspense, Dispatch, SetStateAction } from "react"
 import { motion, useAnimation, useInView } from "framer-motion";
 import { useGLTF } from "@react-three/drei";
 import { Canvas } from "@react-three/fiber";
 import Skill from "@/components/Skill";
-const Atom = lazy(()=> import("@/components/Atom"));
+const Atom = lazy(() => import("@/components/Atom"));
 
 
-function FadeIn({children}: React.PropsWithChildren){
-  const ref=useRef(null)
-  const mainControls= useAnimation();
+
+function FadeIn({ children, setFinished = () => { } }: { children: React.PropsWithChildren, setFinished?: Dispatch<SetStateAction<Boolean>> }) {
+  const ref = useRef(null)
+  const mainControls = useAnimation();
   const isInView = useInView(ref);
-  useEffect(()=>{
+  useEffect(() => {
     if (isInView) mainControls.start("visible")
   }, [isInView])
 
-  const animationConfig={
+  const animationConfig = {
     hidden: {
       opacity: 0, y: 75
     },
@@ -23,29 +24,38 @@ function FadeIn({children}: React.PropsWithChildren){
       opacity: 1, y: 0
     }
   }
+
   return (
-    <motion.div className="w-100 " ref={ref} variants={animationConfig} transition={{ duration: .7, delay: 0.3 }} initial= "hidden" animate={mainControls}>
+    <motion.div
+      className="w-100 "
+      ref={ref}
+      variants={animationConfig}
+      transition={{ duration: .6, delay: 0.3 }}
+      initial="hidden"
+      animate={mainControls}
+      onAnimationComplete={() => setFinished(true)}>
       {children}
     </motion.div>)
 }
-function Clients(){
+
+function Clients({ setFinished }: { setFinished: Dispatch<SetStateAction<Boolean>> }) {
   return (
     <section className="px-10 lg:px-20 pb-20">
-      <FadeIn>
-          <h2 className="text-4xl mb-8">My Clients</h2>
-          <div className="flex flex-col md:flex-row justify-evenly align-center">
-            <div className="h-[50px] flex justify-center">
-              <img src="images/clients/gobuyside.png" alt="goBuySide " className="h-full opacity-25 hover:opacity-100 transition-opacity" height={50}/>
-            </div>
-            <img src="images/clients/zeitfur.svg" alt="Zeit Fui Die Schule" className=" h-[50px] opacity-25 hover:opacity-100 transition-opacity" height={50}/>
+      <FadeIn setFinished={setFinished}>
+        <h2 className="text-4xl mb-8">My Clients</h2>
+        <div className="flex flex-col md:flex-row justify-evenly align-center">
+          <div className="h-[50px] flex justify-center">
+            <img src="images/clients/gobuyside.png" alt="goBuySide " className="h-full opacity-25 hover:opacity-100 transition-opacity" height={50} />
           </div>
+          <img src="images/clients/zeitfur.svg" alt="Zeit Fui Die Schule" className=" h-[50px] opacity-25 hover:opacity-100 transition-opacity" height={50} />
+        </div>
       </FadeIn>
     </section>
   )
 }
 
-function Skills(){
-  const skills=[
+function Skills() {
+  const skills = [
     {
       "image": "images/skills/react.png",
       "title": "React"
@@ -75,10 +85,10 @@ function Skills(){
 
         <h2 className="text-4xl mb-8 ">Skills</h2>
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-5 lg:grid-rows-1  gap-4 mt-36 gap-y-24">
-        {
-          skills.map((skill, i)=>(
+          {
+            skills.map((skill, i) => (
               <Skill {...skill} key={i}></Skill>
-          ))}
+            ))}
         </div>
       </FadeIn>
     </div>
@@ -86,39 +96,36 @@ function Skills(){
 }
 
 export default function Home() {
-  const [title, setTitle] = useState(<>Hi, I&apos;m Andres</>);  
-  const [display, setDisplay]= useState(false);
+  const [title, setTitle] = useState(<>Hi, I&apos;m Andres</>);
+  const [displayAtom, setDisplayAtom] = useState(false);
   const ref = useRef(null);
   const isInView = useInView(ref);
-  const mainControls= useAnimation();
-  useEffect(()=>{
+  const mainControls = useAnimation();
+
+  const [showSkills, setShowSkills] = useState(false)
+
+  useEffect(() => {
     if (isInView) mainControls.start("visible")
   }, [isInView])
 
-  useEffect(()=>{
-    console.log(display)
-  }, [display])
-
-
-  
   return (
 
     <main>
       <div className="lg:flex min-h-screen justify-between py-10 xl:pl-24 bg-background h-100 " ref={ref}>
         <div className="h-100 flex items-center justify-center px-10 sm:px-0">
           <motion.div className="w-100 " variants={{
-              hidden: {
-                opacity: 0, y: 75
-              },
-              visible: {
-                opacity: 1, y: 0
-              }
-            }} 
+            hidden: {
+              opacity: 0, y: 75
+            },
+            visible: {
+              opacity: 1, y: 0
+            }
+          }}
             transition={{ duration: 1, delay: 0.3 }}
-            initial= "hidden"
+            initial="hidden"
             animate={mainControls}
-            onAnimationComplete={()=>{setDisplay(true)}}
-            >
+            onAnimationComplete={() => { setDisplayAtom(true) }}
+          >
             <div className="mb-24 flex flex-col items-center lg:items-start">
               <h1 className="text-center lg:text-left text-5xl xl:text-7xl font-bold mb-2 sm:truncate">
                 {title}
@@ -126,23 +133,23 @@ export default function Home() {
               <p>Full Stack Developer</p>
             </div>
           </motion.div>
-          </div>
-          <div className="h-100 w-100 lg:w-2/3 lg:flex items-center justify-center">
+        </div>
+        <div className="h-100 w-100 lg:w-2/3 lg:flex items-center justify-center">
 
-            <div className="h-[400px] md:h-[550px] lg:h-[600px] xl:h-full xl:w-[80%]  ">
+          <div className="h-[400px] md:h-[550px] lg:h-[600px] xl:h-full xl:w-[80%]  ">
             {
-              display && (
+              displayAtom && (
                 <Canvas>
-
-                <Atom />
+                  <Atom />
                 </Canvas>
               )
             }
-            </div>
+          </div>
         </div>
       </div>
-      <Clients></Clients>
-      <Skills ></Skills>
+      {}
+      <Clients setFinished={setShowSkills}></Clients>
+      {showSkills && <Skills />}
 
     </main>
   );
