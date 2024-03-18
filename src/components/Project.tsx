b2c5e940f73a8fd9fe8bb04391e0b98673a4e280
Andres Gutierrrez <andresgutierrezdev@gmail.com>
2024-03-18 12:02:28 -0400

    adding project component skelleton

---
+++ src/components/Project.tsx
@@ -0,0 +1,10 @@
+import React from "react"
+export default function Project() {
+  return (
+    <div className="rounded p-3">
+      <img src="images/projects/trademanager/main.jpg" alt="" />
+      <h3 className="text-xl">Trade Managerr</h3>
+      <p></p>
+    </div>
+  )
+}
