6885071eaa547e89df2cb96c680c4a54d299d5ad
Andres Gutierrrez <andresgutierrezdev@gmail.com>
2024-03-24 16:52:03 -0400

    commenting stuff on route provider

--- src/app/providers/RouteProvider.tsx
+++ src/app/providers/RouteProvider.tsx
@@ -1,26 +1,21 @@
 "use client"
 
 import { redirect, usePathname } from "next/navigation"
-import { useEffect } from "react";
+import { useEffect, useRef } from "react";
 
-export default function RouteProvider({children}: Readonly<{
+export default function RouteProvider({ children }: Readonly<{
   children: React.ReactNode;
-}>){
+}>) {
 
-  const pathname = usePathname()
-  let i = 0
-  let lastPathname = ""
-  useEffect(() => {
-    if (i == 0) {
-      lastPathname = pathname
-    }
-    if (i== 1){
-      pathname != lastPathname && redirect(pathname)
-      localStorage.setItem("lastPathname", lastPathname)
-      localStorage.setItem("pathname", pathname)
-    }
-    i++
-    // redirect(pathngccame)
-  }, [pathname])
+  // const pathname = usePathname()
+  // const path = useRef("")
+  // useEffect(() => {
+  //   if (pathname !== path.current) path.current = pathname
+  //   // redirect(pathngccame)
+  // }, [pathname])
+  //
+  // useEffect(() => {
+  //   redirect(path.current)
+  // }, [path])
   return children
 }
